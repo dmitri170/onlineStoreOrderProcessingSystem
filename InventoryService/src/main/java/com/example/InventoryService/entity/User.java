bcfc79d98fc62eb39240d63c6efe0e09de18dbc3
d.eremich <dmitri170@yandex.ru>
2025-05-28 17:02:54 +0300

    create

---
+++ InventoryService/src/main/java/com/example/InventoryService/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.InventoryService.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private Role role;
+}
