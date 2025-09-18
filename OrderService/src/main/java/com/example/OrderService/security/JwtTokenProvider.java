322e99f291d08c52657dab0ccc85cae4431661cf
Dmitri <31839513+dmitri170@users.noreply.github.com>
2025-09-18 11:48:32 +0300

    правки

--- OrderService/src/main/java/com/example/OrderService/security/JwtTokenProvider.java
+++ OrderService/src/main/java/com/example/OrderService/security/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenProvider {
 
 
